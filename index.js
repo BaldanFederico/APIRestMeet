a4755b996369de2cb03c5a3d100b659d2965c70b
baldan.federico <baldan.federico@studenti.salesianisesto.it>
2022-01-23 22:27:33 +0100

    add /addMeet

--- index.js
+++ index.js
@@ -82,4 +82,26 @@ res.send(x["name"]+ " , "+x["ADPmeet"]+" ,  "+x["date"]+ " ,  "+x["hour"]);
 });
 });
 
-
+app.post("/addMeet", (req, res) => {
+var username = req.body.username;
+var password = req.body.password;
+var name = req.body.name;
+var ADPmeet = req.body.ADPmeet;
+var date = req.body.date;
+var hour = req.body.hour;
+fs.readFile("user.json", "utf8", (err, jsonString) => {
+if(err) {
+console.log("Error reading file");
+}
+var x = JSON.parse(jsonString);
+if(x["username"]==username && x["password"]==password) {
+var z = ("name: "+name+ " , "+ "ADPmeet: "+ADPmeet+ " , " +" date: "+date+ " , " +"hour: "+hour);
+fs.writeFileSync("meet.json", "    ");
+var y = JSON.stringify(z);
+fs.writeFileSync("meet.json", y);
+res.send("Meet create successful");
+}else{
+res.send("Error");
+}
+});
+});
