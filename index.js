5dcc4aa94a4735584558871db2b3de6485be7e1c
baldan.federico <baldan.federico@studenti.salesianisesto.it>
2022-01-23 21:16:35 +0100

    add /addADP

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const app = express()
+const app = express();
 app.use(express.json());
 const fs = require("fs");
 const port = 3000;
@@ -24,6 +24,30 @@ res.send("Login successful");
 }else{
 res.send("Error");
 //console.log(username);
+}
+});
+});
+
+
+
+app.post("/addADP", (req, res) => {
+var username = req.body.username;
+var password = req.body.password;
+var ADP = req.body.ADP;
+    
+fs.readFile("user.json", "utf8", (err, jsonString) => {
+if(err) {
+console.log("Error reading file");
+}
+var x = JSON.parse(jsonString);
+if(x["username"]==username && x["password"]==password) {
+x["ADP"]=ADP;
+fs.writeFileSync("user.json", "    ");
+var y = JSON.stringify(x);
+fs.writeFileSync("user.json", y);
+res.send("ADP write successful");
+}else{
+res.send("Error");
 }
 });
 });
